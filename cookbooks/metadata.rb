b60706016a3a9eb3902acc06076f19e63f2396f3
Garrett Singer <gesinger@gmail.com>
2014-03-05 20:15:51 -0500

    Started metadata file

---
+++ cookbooks/metadata.rb
@@ -0,0 +1,7 @@
+name        'mongodb-import'
+maintainer  'Garrett Singer'
+license     'MIT'
+description 'Imports data to a mongodb data'
+version     '0.0.1'
+
+recipe 'mongodb-import', 'Imports data to a mongodb instance'
