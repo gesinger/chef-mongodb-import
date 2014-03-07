daa5eabac5dff5372da47518547396fabfe97551
Garrett Singer <gesinger@gmail.com>
2014-03-06 21:09:29 -0500

    Moved metadata.rb

--- cookbooks/metadata.rb
+++
@@ -1,7 +0,0 @@
-name        'mongodb-import'
-maintainer  'Garrett Singer'
-license     'MIT'
-description 'Imports data to a mongodb data'
-version     '0.0.1'
-
-recipe 'mongodb-import', 'Imports data to a mongodb instance'
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mongodb-import'
+maintainer       'Garrett Singer'
+maintainer_email 'gesinger@gmail.com'
+license          'MIT'
+description      'Imports data into a mongodb database'
+version          '0.0.1'
+
+recipe 'mongodb-import', 'Imports data into a mongodb databse'  # TODO hyphen?
