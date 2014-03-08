7ce2713feaf71974b258e0ca2325e331f81779aa
Garrett Singer <gesinger@gmail.com>
2014-03-07 21:23:56 -0500

    Added a few more attributes and execute the command

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,9 @@
+default[:mongodb_import][:host]          = nil
+default[:mongodb_import][:port]          = nil
 default[:mongodb_import][:db]            = nil
 default[:mongodb_import][:dbpath]        = nil
 default[:mongodb_import][:collection]    = nil
 default[:mongodb_import][:file]          = nil
 default[:mongodb_import][:use_journal]   = nil
 default[:mongodb_import][:ignore_blanks] = nil
+default[:mongodb_import][:tail]          = nil
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,31 +1,45 @@
 # Cookbook Name:: mongodb-import
 # Recipe:: default
 
+host          = node[:mongodb_import][:host]
+port          = node[:mongodb_import][:port]
 db            = node[:mongodb_import][:db]
 dbpath        = node[:mongodb_import][:dbpath]
 collection    = node[:mongodb_import][:collection]
 file          = node[:mongodb_import][:file]
 use_journal   = node[:mongodb_import][:use_journal]
 ignore_blanks = node[:mongodb_import][:ignore_blanks]
+tail          = node[:mongodb_import][:tail]
 
-command = "mongoimport"
+import_script = "mongoimport"
+if host
+  import_script += " --host #{host}"
+end
+if port
+  import_script += " --port #{port}"
+end
 if db
-   command += " --db #{db}"
+   import_script += " --db #{db}"
 end
 if dbpath
-   command += " --dbpath #{dbpath}"
+   import_script += " --dbpath #{dbpath}"
 end
 if collection
-   command += " --collection #{collection}"
+   import_script += " --collection #{collection}"
 end
 if use_journal
-   command += " --journal"
+   import_script += " --journal"
 end
 if ignore_blanks
-   command += " --ignoreBlanks"
+   import_script += " --ignoreBlanks"
 end
-command += " --file #{file}"
 
-log "Running command: " + command
+import_script += " --file #{file}"
 
-#action :run
+if tail
+  import_script += " #{tail}"
+end
+
+execute "run import script" do
+  command import_script
+end
